7d4e239e0eec84e891bb3a135595f133bd1ea2fe
wrdsnf <wardafawahana@gmail.com>
2026-02-24 10:44:31 +0700

    edit index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [NAMA] | [NIU]
+            Warda Saniia Fawahaana | 518824
           </h5>
         </div>
       </main>
